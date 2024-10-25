bd41dce4a87fc577f221f540bb97c6663a5f4e19
KANAGALA VENKATA NAGA RAJASEKHAR <109467292+kvnrajasekhar@users.noreply.github.com>
2024-10-25 16:30:09 +0530

    added some padding at top

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
 
   return (
     
-    <nav className="fixed mx-auto border-0 border-[#33353F] top-0 left-0 right-0 z-20  bg-[#121212] bg-opacity-100 py-0">
+    <nav className="fixed mx-auto border-0 border-[#33353F] top-0 left-0 right-0 z-20  bg-[#121212] bg-opacity-100 py-6">
       <div className="flex container flex-wrap items-center justify-between mx-auto px-4 py-0">
         <Link
           href={"/"}
