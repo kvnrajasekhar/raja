5c825709dbe95c63d1916b7b798abd225af4db38
KANAGALA VENKATA NAGA RAJASEKHAR <109467292+kvnrajasekhar@users.noreply.github.com>
2024-11-19 23:52:11 +0530

    removed fixed property for nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,45 +28,47 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    
-    <nav className="fixed mx-auto border-0 border-[#33353F] top-0 left-0 right-0 z-20  bg-[#121212] bg-opacity-100 py-3">
-      <div className="flex container flex-wrap items-center justify-between mx-auto px-4 py-0">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-3xl text-white font-semibold no-underline"
-        >
-          Portfolio
-        </Link>
-        <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
-            <button
-              onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
+    <>
+      <nav className=" top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-100 py-2 border-b border-[#33353F]">
+        <div className="flex container items-center justify-between mx-auto px-4 py-0">
+          {/* Logo on the left */}
+          <Link href={"/"} className="text-2xl md:text-3xl text-white font-semibold no-underline">
+            Portfolio
+          </Link>
+
+          {/* Mobile menu toggle */}
+          <div className="mobile-menu block md:hidden">
+            {!navbarOpen ? (
+              <button
+                onClick={() => setNavbarOpen(true)}
+                className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              >
+                <Bars3Icon className="h-5 w-5" />
+              </button>
+            ) : (
+              <button
+                onClick={() => setNavbarOpen(false)}
+                className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              >
+                <XMarkIcon className="h-5 w-5" />
+              </button>
+            )}
+          </div>
+
+          {/* Navigation Links on the right */}
+          <div className="menu hidden md:block md:w-auto px-6">
+            <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 py-0">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink href={link.path} title={link.title} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="menu hidden md:block md:w-auto px-6" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 py-0">
-            {navLinks.map((link, index) => (
-              <li key={index}>
-                <NavLink href={link.path} title={link.title} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-    </nav>
-    
+        {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      </nav>
+    </>
   );
 };
 
